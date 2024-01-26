ca2551513395a9d2cba6249ca2d069560800d120
Pavel-Masharov <leonmasharov@gmail.com>
2024-01-26 16:46:31 +0500

    add initial-window without code changes

---
+++ Assets/Scripts/UI/InitialWindow.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using CookingPrototype.Controllers;
+
+namespace CookingPrototype.UI {
+	public class InitialWindow : MonoBehaviour {
+
+		[SerializeField] private TMP_Text _taskText;
+		[SerializeField] private Button _playButton;
+
+		private void Start() {
+			GameplayController.Instance.TotalOrdersServedChanged += ShowInitialWindow;
+			gameObject.SetActive(false);
+		}
+
+		public void ShowInitialWindow() {
+			string wordsTask = "Приготовь и передай $ блюд.";
+			string resultWords = wordsTask.Replace("$", GameplayController.Instance.OrdersTarget.ToString());
+			_taskText.text = resultWords;
+			_playButton.onClick.AddListener(StartGame);
+			gameObject.SetActive(true);
+			Time.timeScale = 0f;
+		}
+
+		private void StartGame() {
+			gameObject.SetActive(false);
+			Time.timeScale = 1f;
+			_playButton.onClick.RemoveListener(StartGame);
+			GameplayController.Instance.TotalOrdersServedChanged -= ShowInitialWindow;
+		}
+	}
+}
